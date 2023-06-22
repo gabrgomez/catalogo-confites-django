6e7b4ad4bc50b4619fa88e260e19ab82044912f5
Gabriela Gomez <128086087+gabrgomez@users.noreply.github.com>
2023-06-22 00:15:55 -0400

    configurando login url

--- conf/settings.py
+++ conf/settings.py
@@ -135,6 +135,8 @@ BASE_DIR / 'media',]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+LOGIN_URL='login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
